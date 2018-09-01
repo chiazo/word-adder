b2f63bd51b027c8f616a07a86cb9e33402d2d147
chiazo <chiazo@live.unc.edu>
2018-09-01 03:01:14 -0400

    includes addWords method

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import edu.duke.URLResource;
+
+class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //private URLResource
+
+
+
+
+    }
+}
---
+++ src/addWords.java
@@ -0,0 +1,23 @@
+import edu.duke.URLResource;
+
+import java.util.HashMap;
+
+public class addWords {
+
+    public addWords(){
+        URLResource url = new URLResource("http://www.igbofocus.co.uk/Igbo-Language/Learn-Some-Every-Day-Igbo-Word/learn-some-every-day-igbo-words.html");
+        HashMap<HashMap<String, String>, HashMap<String, String>> wordMap = new HashMap<>();
+        String bold = "";
+        String definition = "";
+        String line = "";
+        int index = 0;
+    }
+
+
+
+
+
+
+}
+
+
