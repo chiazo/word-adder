0a3019a7992329db00b7055f2e30ca2834d742c8
chiazo <chiazo@live.unc.edu>
2018-09-03 20:28:50 -0400

    new class to process each word/sentence with matching translation

---
+++ src/Words.java
@@ -0,0 +1,15 @@
+public class Words {
+
+    private String english;
+    private String igbo;
+    private String englSentence;
+    private String igboSentence;
+
+    public Words(){
+        english = "";
+        igbo = "";
+        englSentence = "";
+        igboSentence = "";
+    }
+
+}
